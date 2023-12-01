1a236e754b5e807246f8e44792ec4a6af1b6db8d
Davide Luppoli <davide@davideluppoli.it>
2023-12-01 12:34:49 +0100

    Add project files.

---
+++ WcfClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfClient.ServiceReference;
+
+namespace WcfClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Service1Client();
+            string risultato1 = client.GetData(12);
+            Console.WriteLine(risultato1);
+
+            CompositeType compositeType = new CompositeType
+            {
+                BoolValue = true,
+                StringValue = "Hello World!"
+            };
+            var risultato2 = client.GetDataUsingDataContract(compositeType);
+            Console.WriteLine($"{risultato2.BoolValue} - {risultato2.StringValue}");
+
+            Console.ReadLine();
+        }
+    }
+}
